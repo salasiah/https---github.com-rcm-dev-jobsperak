7c0bda38526ea919a23a82937e20a2bb57ede575
richcoremedia <mahfudz@richcoremedia.com>
2012-06-06 23:55:45 +0800

    * Added nav, content and sidebar

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8" />
 	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-	<title></title>
+	<title>Welcome to Jobsperak Portal</title>
 	<meta name="keywords" content="" />
 	<meta name="description" content="" />
 	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen, projection" />
@@ -11,37 +11,63 @@
 
 <body>
 
-<div id="wrapper">
+
 
 	<header id="header">
-		<nav>
-        	<ul>
-            	<li><a href="#">Home</a></li>
-                <li><a href="#">Search</a></li>
-                <li><a href="#">Register</a></li>
-            </ul>
+
+		<div class="center">
+			<h1>Jobs Perak</h1>
+		</div><!-- .center -->
+		
+		<nav id="menu">
+			<div class="center">
+	        	<ul id="navigation">
+	            	<li><a href="#">Home</a></li>
+	                <li><a href="#">Search</a></li>
+	                <li><a href="#">Register</a></li>
+	            </ul>
+            </div><!-- .center -->
         </nav>
 	</header><!-- #header-->
 
+	<div id="wrapper">
+	
 	<section id="middle">
 
 		<div id="container">
 			<div id="content">
-				<strong>Content:</strong> Sed placerat accumsan ligula. Aliquam felis magna, congue quis, tempus eu, aliquam vitae, ante. Cras neque justo, ultrices at, rhoncus a, facilisis eget, nisl. Quisque vitae pede. Nam et augue. Sed a elit. Ut vel massa. Suspendisse nibh pede, ultrices vitae, ultrices nec, mollis non, nibh. In sit amet pede quis leo vulputate hendrerit. Cras laoreet leo et justo auctor condimentum. Integer id enim. Suspendisse egestas, dui ac egestas mollis, libero orci hendrerit lacus, et malesuada lorem neque ac libero. Morbi tempor pulvinar pede. Donec vel elit.
-			</div><!-- #content-->
-		</div><!-- #container-->
+			  <div class="browse_location box">
+			    <h2 class="title">Browse by Location</h2>
+			  </div>
+			  <div class="browse_industry box">
+			    <h2 class="title">Browse by Industry</h2>
+			  </div>
+			  <div class="freatured_employer box">
+			    <h2 class="title">Featured Employer</h2>
+			  </div>
+			  <div class="title">
+			    <h2>Featured Candidate</h2>
+			  </div>
+            </div><!-- #content-->
+	
+			<aside id="sideRight">
+				<strong>Right Sidebar:</strong> Integer velit. Vestibulum nisi nunc, accumsan ut, vehicula sit amet, porta a, mi. Nam nisl tellus, placerat eget, posuere eget, egestas eget, dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In elementum urna a eros. Integer iaculis. Maecenas vel elit.
+			</aside><!-- #sideRight -->
 
-		<aside id="sideRight">
-			<strong>Right Sidebar:</strong> Integer velit. Vestibulum nisi nunc, accumsan ut, vehicula sit amet, porta a, mi. Nam nisl tellus, placerat eget, posuere eget, egestas eget, dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In elementum urna a eros. Integer iaculis. Maecenas vel elit.
-		</aside><!-- #sideRight -->
+		</div><!-- #container-->
+		
 
 	</section><!-- #middle-->
 
+	</div><!-- #wrapper-->
+
 	<footer id="footer">
-		<strong>Footer:</strong> Mus elit Morbi mus enim lacus at quis Nam eget morbi. Et semper urna urna non at cursus dolor vestibulum neque enim. Tellus interdum at laoreet laoreet lacinia lacinia sed Quisque justo quis. Hendrerit scelerisque lorem elit orci tempor tincidunt enim Phasellus dignissim tincidunt. Nunc vel et Sed nisl Vestibulum odio montes Aliquam volutpat pellentesque. Ut pede sagittis et quis nunc gravida porttitor ligula.
+		<div class="center">
+			Copyright Reserved &copy; 2012
+		</div><!-- .center -->
 	</footer><!-- #footer -->
 
-</div><!-- #wrapper -->
+
 
 </body>
 </html>
