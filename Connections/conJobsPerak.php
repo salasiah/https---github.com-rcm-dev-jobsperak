983a4cbacb73752ba947d18de4566c8eaa23be55
richcoremedia <mahfudz@richcoremedia.com>
2012-06-07 00:11:21 +0800

    * Added jobsperak db

---
+++ Connections/conJobsPerak.php
@@ -0,0 +1,10 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_conJobsPerak = "localhost";
+$database_conJobsPerak = "rcm_jobsperak";
+$username_conJobsPerak = "root";
+$password_conJobsPerak = "";
+$conJobsPerak = mysql_pconnect($hostname_conJobsPerak, $username_conJobsPerak, $password_conJobsPerak) or trigger_error(mysql_error(),E_USER_ERROR); 
+?>
