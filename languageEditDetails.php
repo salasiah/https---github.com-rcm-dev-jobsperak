982465bb0d58823e388b0b43f23d82ed293e4ead
richcoremedia <mahfudz@richcoremedia.com>
2012-06-09 21:01:21 +0800

    * Added language, references and skill page update

---
+++ languageEditDetails.php
@@ -0,0 +1,323 @@
+<?php require_once('Connections/conJobsPerak.php'); ?>
+<?php
+//initialize the session
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+// ** Logout the current user. **
+$logoutAction = $_SERVER['PHP_SELF']."?doLogout=true";
+if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")){
+  $logoutAction .="&". htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
+  //to fully log out a visitor we need to clear the session varialbles
+  $_SESSION['MM_Username'] = NULL;
+  $_SESSION['MM_UserGroup'] = NULL;
+  $_SESSION['PrevUrl'] = NULL;
+  unset($_SESSION['MM_Username']);
+  unset($_SESSION['MM_UserGroup']);
+  unset($_SESSION['PrevUrl']);
+	
+  $logoutGoTo = "login.php";
+  if ($logoutGoTo) {
+    header("Location: $logoutGoTo");
+    exit;
+  }
+}
+?>
+<?php
+if (!isset($_SESSION)) {
+  session_start();
+}
+$MM_authorizedUsers = "1";
+$MM_donotCheckaccess = "false";
+
+// *** Restrict Access To Page: Grant or deny access to this page
+function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
+  // For security, start by assuming the visitor is NOT authorized. 
+  $isValid = False; 
+
+  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
+  // Therefore, we know that a user is NOT logged in if that Session variable is blank. 
+  if (!empty($UserName)) { 
+    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login. 
+    // Parse the strings into arrays. 
+    $arrUsers = Explode(",", $strUsers); 
+    $arrGroups = Explode(",", $strGroups); 
+    if (in_array($UserName, $arrUsers)) { 
+      $isValid = true; 
+    } 
+    // Or, you may restrict access to only certain users based on their username. 
+    if (in_array($UserGroup, $arrGroups)) { 
+      $isValid = true; 
+    } 
+    if (($strUsers == "") && false) { 
+      $isValid = true; 
+    } 
+  } 
+  return $isValid; 
+}
+
+$MM_restrictGoTo = "sessionGateway.php";
+if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
+  $MM_qsChar = "?";
+  $MM_referrer = $_SERVER['PHP_SELF'];
+  if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
+  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0) 
+  $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
+  $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
+  header("Location: ". $MM_restrictGoTo); 
+  exit;
+}
+?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "form1")) {
+  $updateSQL = sprintf("UPDATE jp_language SET lang_name=%s, lang_written=%s, lang_spoken=%s, user_id_fk=%s WHERE lang_id=%s",
+                       GetSQLValueString($_POST['lang_name'], "text"),
+                       GetSQLValueString($_POST['lang_written'], "int"),
+                       GetSQLValueString($_POST['lang_spoken'], "int"),
+                       GetSQLValueString($_POST['user_id_fk'], "int"),
+                       GetSQLValueString($_POST['lang_id'], "int"));
+
+  mysql_select_db($database_conJobsPerak, $conJobsPerak);
+  $Result1 = mysql_query($updateSQL, $conJobsPerak) or die(mysql_error());
+
+  $updateGoTo = "jobSeekerMyResume.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $updateGoTo .= (strpos($updateGoTo, '?')) ? "&" : "?";
+    $updateGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $updateGoTo));
+}
+
+mysql_select_db($database_conJobsPerak, $conJobsPerak);
+$query_rsIndustryList = "SELECT * FROM jp_industry";
+$rsIndustryList = mysql_query($query_rsIndustryList, $conJobsPerak) or die(mysql_error());
+$row_rsIndustryList = mysql_fetch_assoc($rsIndustryList);
+$totalRows_rsIndustryList = mysql_num_rows($rsIndustryList);
+
+mysql_select_db($database_conJobsPerak, $conJobsPerak);
+$query_rsSpecializeList = "SELECT * FROM jp_specialize";
+$rsSpecializeList = mysql_query($query_rsSpecializeList, $conJobsPerak) or die(mysql_error());
+$row_rsSpecializeList = mysql_fetch_assoc($rsSpecializeList);
+$totalRows_rsSpecializeList = mysql_num_rows($rsSpecializeList);
+
+mysql_select_db($database_conJobsPerak, $conJobsPerak);
+$query_rsLevelList = "SELECT * FROM jp_level";
+$rsLevelList = mysql_query($query_rsLevelList, $conJobsPerak) or die(mysql_error());
+$row_rsLevelList = mysql_fetch_assoc($rsLevelList);
+$totalRows_rsLevelList = mysql_num_rows($rsLevelList);
+
+mysql_select_db($database_conJobsPerak, $conJobsPerak);
+$query_rsLanguList = "SELECT * FROM jp_language_list";
+$rsLanguList = mysql_query($query_rsLanguList, $conJobsPerak) or die(mysql_error());
+$row_rsLanguList = mysql_fetch_assoc($rsLanguList);
+$totalRows_rsLanguList = mysql_num_rows($rsLanguList);
+
+$colname_rsUserLanguage = "-1";
+if (isset($_GET['lang_id'])) {
+  $colname_rsUserLanguage = $_GET['lang_id'];
+}
+mysql_select_db($database_conJobsPerak, $conJobsPerak);
+$query_rsUserLanguage = sprintf("SELECT * FROM jp_language WHERE lang_id = %s", GetSQLValueString($colname_rsUserLanguage, "int"));
+$rsUserLanguage = mysql_query($query_rsUserLanguage, $conJobsPerak) or die(mysql_error());
+$row_rsUserLanguage = mysql_fetch_assoc($rsUserLanguage);
+$totalRows_rsUserLanguage = mysql_num_rows($rsUserLanguage);
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8" />
+	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<title>Welcome to Jobsperak Portal</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen, projection" />
+</head>
+
+<body>
+
+
+
+	<header id="header">
+
+		<div class="center">
+			<div class="left">
+				<h1>Jobs Perak</h1>
+			</div>
+
+			<div class="right">
+				<a href="#" title="Login">Login</a> &nbsp;|&nbsp;
+                <a href="#" title="Register">Register</a>
+			</div>
+			<div class="clear"></div>
+		</div><!-- .center -->
+		
+		<nav id="menu">
+			<div class="center">
+	        	<ul id="navigation">
+	            	<li><a href="index.php">Home</a></li>
+	                <li><a href="#">Search</a></li>
+	                <li><a href="#">Register</a></li>
+                    <li><a href="#">Employer : Post a Job</a></li>
+	            </ul>
+            </div><!-- .center -->
+        </nav>
+	</header><!-- #header-->
+
+	<div id="wrapper">
+	
+	<section id="middle">
+
+		<div id="container" class="full">
+		  <div id="content_full">
+<h2>Update</h2>
+<div class="master_details">
+  <p>Welcome <?php echo $_SESSION['MM_Username']; ?> <?php echo $_SESSION['MM_UserID']; ?> | <a href="<?php echo $logoutAction ?>">Log Out</a></p>
+  
+  <div class="master_details boxcenter">
+	<h3>Update Language</h3><br/>
+	<form action="<?php echo $editFormAction; ?>" method="POST" name="form1">
+	  <table align="center">
+        <tr valign="baseline">
+          <td nowrap align="right">Language:</td>
+          <td><select name="lang_name">
+            <?php
+do {  
+?>
+            <option value="<?php echo $row_rsLanguList['languList_id']?>"><?php echo $row_rsLanguList['languList_name']?></option>
+            <?php
+} while ($row_rsLanguList = mysql_fetch_assoc($rsLanguList));
+  $rows = mysql_num_rows($rsLanguList);
+  if($rows > 0) {
+      mysql_data_seek($rsLanguList, 0);
+	  $row_rsLanguList = mysql_fetch_assoc($rsLanguList);
+  }
+?>
+          </select></td>
+        <tr>
+        <tr valign="baseline">
+          <td nowrap align="right">Written</td>
+          <td><input type="text" name="lang_written" value="<?php echo $row_rsUserLanguage['lang_written']; ?>" size="32"></td>
+        </tr>
+        <tr valign="baseline">
+          <td nowrap align="right">Spoken</td>
+          <td><input type="text" name="lang_spoken" value="<?php echo $row_rsUserLanguage['lang_spoken']; ?>" size="32"></td>
+        </tr>
+        <tr valign="baseline">
+          <td nowrap align="right">&nbsp;</td>
+          <td><input type="submit" value="Update Language"></td>
+        </tr>
+      </table>
+      <input type="hidden" name="lang_id" value="<?php echo $row_rsUserLanguage['lang_id']; ?>">
+      <input type="hidden" name="user_id_fk" value="<?php echo $_SESSION['MM_UserID']; ?>">
+      <input type="hidden" name="MM_update" value="form1">
+    </form>
+    <p>&nbsp;</p>
+<p>&nbsp;</p>
+  </div>
+</div>
+
+          </div><!-- #content-->
+	
+		  <aside id="sideRight" class="hide">
+          	  <div class="sidebarBox">
+              	<strong>How-to</strong>
+            	<div class="sidebar_howto">
+                	<ul>
+                    	<li><a href="#">Register</a></li>
+                        <li><a href="#">Post a Job</a></li>
+                    </ul>
+	            </div><!-- .sidebar_recentjob -->
+              </div><!-- .sidebarBox -->
+              
+			  <div class="sidebarBox hide">
+              	<strong>Recent Jobs</strong>
+            	<div class="sidebar_recentjob">
+                	<ul>
+                      <li><a></a></li>
+                    </ul>
+	            </div><!-- .sidebar_recentjob -->
+              </div><!-- .sidebarBox -->
+              
+              <div class="sidebarBox hide">
+           	  <strong>Jobs Posted under </strong>
+              	<ul>
+                  <li><a></a></li>
+                </ul>
+              </div><!-- .sidebarBox -->
+              
+              <div class="sidebarBox hide">
+           	  <strong>Get Connected</strong><br />
+              	Facebook | Twitter | RSS
+              </div><!-- .sidebarBox -->
+            </aside>
+			<!-- aside -->
+			<!-- #sideRight -->
+
+		</div><!-- #container-->
+		
+
+	</section><!-- #middle-->
+
+	</div><!-- #wrapper-->
+
+	<footer id="footer">
+		<div class="center">
+			Copyright Reserved &copy; 2012
+		</div><!-- .center -->
+	</footer><!-- #footer -->
+
+
+
+</body>
+</html>
+<?php
+mysql_free_result($rsIndustryList);
+
+mysql_free_result($rsSpecializeList);
+
+mysql_free_result($rsLevelList);
+
+mysql_free_result($rsLanguList);
+
+mysql_free_result($rsUserLanguage);
+?>
---
+++ skillsEdit.php
@@ -0,0 +1,249 @@
+<?php require_once('Connections/conJobsPerak.php'); ?>
+<?php
+//initialize the session
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+// ** Logout the current user. **
+$logoutAction = $_SERVER['PHP_SELF']."?doLogout=true";
+if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")){
+  $logoutAction .="&". htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
+  //to fully log out a visitor we need to clear the session varialbles
+  $_SESSION['MM_Username'] = NULL;
+  $_SESSION['MM_UserGroup'] = NULL;
+  $_SESSION['PrevUrl'] = NULL;
+  unset($_SESSION['MM_Username']);
+  unset($_SESSION['MM_UserGroup']);
+  unset($_SESSION['PrevUrl']);
+	
+  $logoutGoTo = "login.php";
+  if ($logoutGoTo) {
+    header("Location: $logoutGoTo");
+    exit;
+  }
+}
+?>
+<?php
+if (!isset($_SESSION)) {
+  session_start();
+}
+$MM_authorizedUsers = "1";
+$MM_donotCheckaccess = "false";
+
+// *** Restrict Access To Page: Grant or deny access to this page
+function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
+  // For security, start by assuming the visitor is NOT authorized. 
+  $isValid = False; 
+
+  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
+  // Therefore, we know that a user is NOT logged in if that Session variable is blank. 
+  if (!empty($UserName)) { 
+    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login. 
+    // Parse the strings into arrays. 
+    $arrUsers = Explode(",", $strUsers); 
+    $arrGroups = Explode(",", $strGroups); 
+    if (in_array($UserName, $arrUsers)) { 
+      $isValid = true; 
+    } 
+    // Or, you may restrict access to only certain users based on their username. 
+    if (in_array($UserGroup, $arrGroups)) { 
+      $isValid = true; 
+    } 
+    if (($strUsers == "") && false) { 
+      $isValid = true; 
+    } 
+  } 
+  return $isValid; 
+}
+
+$MM_restrictGoTo = "sessionGateway.php";
+if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
+  $MM_qsChar = "?";
+  $MM_referrer = $_SERVER['PHP_SELF'];
+  if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
+  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0) 
+  $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
+  $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
+  header("Location: ". $MM_restrictGoTo); 
+  exit;
+}
+?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$colname_rsUserSkills = "-1";
+if (isset($_SESSION['MM_UserID'])) {
+  $colname_rsUserSkills = $_SESSION['MM_UserID'];
+}
+mysql_select_db($database_conJobsPerak, $conJobsPerak);
+$query_rsUserSkills = sprintf("SELECT * FROM jp_skills WHERE user_id_fk = %s", GetSQLValueString($colname_rsUserSkills, "int"));
+$rsUserSkills = mysql_query($query_rsUserSkills, $conJobsPerak) or die(mysql_error());
+$row_rsUserSkills = mysql_fetch_assoc($rsUserSkills);
+$totalRows_rsUserSkills = mysql_num_rows($rsUserSkills);
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8" />
+	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<title>Welcome to Jobsperak Portal</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen, projection" />
+</head>
+
+<body>
+
+
+
+	<header id="header">
+
+		<div class="center">
+			<div class="left">
+				<h1>Jobs Perak</h1>
+			</div>
+
+			<div class="right">
+				<a href="#" title="Login">Login</a> &nbsp;|&nbsp;
+                <a href="#" title="Register">Register</a>
+			</div>
+			<div class="clear"></div>
+		</div><!-- .center -->
+		
+		<nav id="menu">
+			<div class="center">
+	        	<ul id="navigation">
+	            	<li><a href="index.php">Home</a></li>
+	                <li><a href="#">Search</a></li>
+	                <li><a href="#">Register</a></li>
+                    <li><a href="#">Employer : Post a Job</a></li>
+	            </ul>
+            </div><!-- .center -->
+        </nav>
+	</header><!-- #header-->
+
+	<div id="wrapper">
+	
+	<section id="middle">
+
+		<div id="container" class="full">
+		  <div id="content_full">
+<h2>Update</h2>
+<div class="master_details">
+  <p>Welcome <?php echo $_SESSION['MM_Username']; ?> <?php echo $_SESSION['MM_UserID']; ?> | <a href="<?php echo $logoutAction ?>">Log Out</a></p>
+  
+  <div class="master_details boxcenter">
+	<h3>List Experience</h3><br/>
+	<p>There has <?php echo $totalRows_rsUserSkills ?>&nbsp;  at your resume. <a href="#">Add New</a> </p>
+    <table width="700" border="0" align="center" cellpadding="4" cellspacing="0" class="csstable">
+      <tr>
+        <th>Skills</th>
+        <th>Year Experiences</th>
+        <th> Proficiency </th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+      <?php do { ?>
+        <tr>
+          <td align="center" valign="middle"><?php echo $row_rsUserSkills['skills_name']; ?></td>
+          <td align="center" valign="middle"><?php echo $row_rsUserSkills['skills_y_exp']; ?></td>
+          <td align="center" valign="middle"><?php echo $row_rsUserSkills['skills_proficiency']; ?></td>
+          <td align="center" valign="middle"><a href="skillsEditDetails.php?skills_id=<?php echo $row_rsUserSkills['skills_id']; ?>&uid=<?php echo $_SESSION['MM_UserID']; ?>">Edit</a></td>
+          <td align="center" valign="middle"><a href="#">Delete</a></td>
+        </tr>
+        <?php } while ($row_rsUserSkills = mysql_fetch_assoc($rsUserSkills)); ?>
+    </table>
+    <br/>
+    <a href="skillsAdd.php?cuid=<?php echo $_SESSION['MM_UserID']; ?>">Add More Experience</a> </div>
+</div>
+
+          </div><!-- #content-->
+	
+		  <aside id="sideRight" class="hide">
+          	  <div class="sidebarBox">
+              	<strong>How-to</strong>
+            	<div class="sidebar_howto">
+                	<ul>
+                    	<li><a href="#">Register</a></li>
+                        <li><a href="#">Post a Job</a></li>
+                    </ul>
+	            </div><!-- .sidebar_recentjob -->
+              </div><!-- .sidebarBox -->
+              
+			  <div class="sidebarBox hide">
+              	<strong>Recent Jobs</strong>
+            	<div class="sidebar_recentjob">
+                	<ul>
+                      <li><a></a></li>
+                    </ul>
+	            </div><!-- .sidebar_recentjob -->
+              </div><!-- .sidebarBox -->
+              
+              <div class="sidebarBox hide">
+           	  <strong>Jobs Posted under </strong>
+              	<ul>
+                  <li><a></a></li>
+                </ul>
+              </div><!-- .sidebarBox -->
+              
+              <div class="sidebarBox hide">
+           	  <strong>Get Connected</strong><br />
+              	Facebook | Twitter | RSS
+              </div><!-- .sidebarBox -->
+            </aside>
+			<!-- aside -->
+			<!-- #sideRight -->
+
+		</div><!-- #container-->
+		
+
+	</section><!-- #middle-->
+
+	</div><!-- #wrapper-->
+
+	<footer id="footer">
+		<div class="center">
+			Copyright Reserved &copy; 2012
+		</div><!-- .center -->
+	</footer><!-- #footer -->
+
+
+
+</body>
+</html>
+<?php
+mysql_free_result($rsUserSkills);
+?>
