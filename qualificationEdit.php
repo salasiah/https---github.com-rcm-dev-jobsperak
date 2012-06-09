04e0d2baa84961e6aa16c5bf9fac7bd16a279aab
richcoremedia <mahfudz@richcoremedia.com>
2012-06-09 18:39:11 +0800

    * Added Quali Edit

---
+++ qualificationEdit.php
@@ -0,0 +1,251 @@
+<?php require_once('Connections/conJobsPerak.php'); ?>
+<?php
+//initialize the session
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+// ** Logout the current user. **
+$logoutAction = $_SERVER['PHP_SELF']."?doLogout=true";
+if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")){
+  $logoutAction .="&". htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
+  //to fully log out a visitor we need to clear the session varialbles
+  $_SESSION['MM_Username'] = NULL;
+  $_SESSION['MM_UserGroup'] = NULL;
+  $_SESSION['PrevUrl'] = NULL;
+  unset($_SESSION['MM_Username']);
+  unset($_SESSION['MM_UserGroup']);
+  unset($_SESSION['PrevUrl']);
+	
+  $logoutGoTo = "login.php";
+  if ($logoutGoTo) {
+    header("Location: $logoutGoTo");
+    exit;
+  }
+}
+?>
+<?php
+if (!isset($_SESSION)) {
+  session_start();
+}
+$MM_authorizedUsers = "1";
+$MM_donotCheckaccess = "false";
+
+// *** Restrict Access To Page: Grant or deny access to this page
+function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
+  // For security, start by assuming the visitor is NOT authorized. 
+  $isValid = False; 
+
+  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
+  // Therefore, we know that a user is NOT logged in if that Session variable is blank. 
+  if (!empty($UserName)) { 
+    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login. 
+    // Parse the strings into arrays. 
+    $arrUsers = Explode(",", $strUsers); 
+    $arrGroups = Explode(",", $strGroups); 
+    if (in_array($UserName, $arrUsers)) { 
+      $isValid = true; 
+    } 
+    // Or, you may restrict access to only certain users based on their username. 
+    if (in_array($UserGroup, $arrGroups)) { 
+      $isValid = true; 
+    } 
+    if (($strUsers == "") && false) { 
+      $isValid = true; 
+    } 
+  } 
+  return $isValid; 
+}
+
+$MM_restrictGoTo = "sessionGateway.php";
+if (!((isset($_SESSION['MM_Username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['MM_Username'], $_SESSION['MM_UserGroup'])))) {   
+  $MM_qsChar = "?";
+  $MM_referrer = $_SERVER['PHP_SELF'];
+  if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
+  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0) 
+  $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
+  $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
+  header("Location: ". $MM_restrictGoTo); 
+  exit;
+}
+?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$colname_rsUserQualification = "-1";
+if (isset($_SESSION['MM_UserID'])) {
+  $colname_rsUserQualification = $_SESSION['MM_UserID'];
+}
+mysql_select_db($database_conJobsPerak, $conJobsPerak);
+$query_rsUserQualification = sprintf("Select   jp_edu_lists.edu_name,   jp_field_list.field_name,   jp_education.*,   jp_grade_list.grade_name,   jp_nationality.national_name From   jp_education Inner Join   jp_edu_lists On jp_education.edu_qualification = jp_edu_lists.edu_id   Inner Join   jp_field_list On jp_education.edu_fieldStudy = jp_field_list.field_id   Inner Join   jp_grade_list On jp_education.edu_grade = jp_grade_list.grade_id Inner Join   jp_nationality On jp_education.edu_located = jp_nationality.national_id Where   jp_education.user_id_fk = %s", GetSQLValueString($colname_rsUserQualification, "int"));
+$rsUserQualification = mysql_query($query_rsUserQualification, $conJobsPerak) or die(mysql_error());
+$row_rsUserQualification = mysql_fetch_assoc($rsUserQualification);
+$totalRows_rsUserQualification = mysql_num_rows($rsUserQualification);
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8" />
+	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<title>Welcome to Jobsperak Portal</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen, projection" />
+</head>
+
+<body>
+
+
+
+	<header id="header">
+
+		<div class="center">
+			<div class="left">
+				<h1>Jobs Perak</h1>
+			</div>
+
+			<div class="right">
+				<a href="#" title="Login">Login</a> &nbsp;|&nbsp;
+                <a href="#" title="Register">Register</a>
+			</div>
+			<div class="clear"></div>
+		</div><!-- .center -->
+		
+		<nav id="menu">
+			<div class="center">
+	        	<ul id="navigation">
+	            	<li><a href="index.php">Home</a></li>
+	                <li><a href="#">Search</a></li>
+	                <li><a href="#">Register</a></li>
+                    <li><a href="#">Employer : Post a Job</a></li>
+	            </ul>
+            </div><!-- .center -->
+        </nav>
+	</header><!-- #header-->
+
+	<div id="wrapper">
+	
+	<section id="middle">
+
+		<div id="container" class="full">
+		  <div id="content_full">
+<h2>Update</h2>
+<div class="master_details">
+  <p>Welcome <?php echo $_SESSION['MM_Username']; ?> <?php echo $_SESSION['MM_UserID']; ?> | <a href="<?php echo $logoutAction ?>">Log Out</a></p>
+  
+  <div class="master_details boxcenter">
+	<h3>List Education</h3><br/>
+	<p>There has <?php echo $totalRows_rsUserQualification ?> at your resume.
+      <?php if ($totalRows_rsUserQualification == 0) { // Show if recordset empty ?>
+  <a href="qualificationAdd.php?cuid=<?php echo $_SESSION['MM_UserID']; ?>">Add New</a>
+  <?php } // Show if recordset empty ?>
+    </p>
+    <table width="700" border="0" align="center" cellpadding="4" cellspacing="0" class="csstable">
+      <tr>
+        <th>Qualification</th>
+        <th>Major</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+      <?php do { ?>
+        <tr>
+          <td><?php echo $row_rsUserQualification['edu_name']; ?></td>
+          <td><?php echo $row_rsUserQualification['edu_major']; ?></td>
+          <td><a href="qualificationEditDetail.php?edu_id=<?php echo $row_rsUserQualification['edu_id']; ?>&uid=<?php echo $_SESSION['MM_UserID']; ?>">Edit</a></td>
+          <td><a href="#">Delete</a></td>	
+        </tr>
+        <?php } while ($row_rsUserQualification = mysql_fetch_assoc($rsUserQualification)); ?>
+    </table>
+    <br/>
+    <a href="qualificationAdd.php?cuid=<?php echo $_SESSION['MM_UserID']; ?>">Add More Qualification</a> </div>
+</div>
+
+          </div><!-- #content-->
+	
+		  <aside id="sideRight" class="hide">
+          	  <div class="sidebarBox">
+              	<strong>How-to</strong>
+            	<div class="sidebar_howto">
+                	<ul>
+                    	<li><a href="#">Register</a></li>
+                        <li><a href="#">Post a Job</a></li>
+                    </ul>
+	            </div><!-- .sidebar_recentjob -->
+              </div><!-- .sidebarBox -->
+              
+			  <div class="sidebarBox hide">
+              	<strong>Recent Jobs</strong>
+            	<div class="sidebar_recentjob">
+                	<ul>
+                      <li><a></a></li>
+                    </ul>
+	            </div><!-- .sidebar_recentjob -->
+              </div><!-- .sidebarBox -->
+              
+              <div class="sidebarBox hide">
+           	  <strong>Jobs Posted under </strong>
+              	<ul>
+                  <li><a></a></li>
+                </ul>
+              </div><!-- .sidebarBox -->
+              
+              <div class="sidebarBox hide">
+           	  <strong>Get Connected</strong><br />
+              	Facebook | Twitter | RSS
+              </div><!-- .sidebarBox -->
+            </aside>
+			<!-- aside -->
+			<!-- #sideRight -->
+
+		</div><!-- #container-->
+		
+
+	</section><!-- #middle-->
+
+	</div><!-- #wrapper-->
+
+	<footer id="footer">
+		<div class="center">
+			Copyright Reserved &copy; 2012
+		</div><!-- .center -->
+	</footer><!-- #footer -->
+
+
+
+</body>
+</html>
+<?php
+mysql_free_result($rsUserQualification);
+?>
